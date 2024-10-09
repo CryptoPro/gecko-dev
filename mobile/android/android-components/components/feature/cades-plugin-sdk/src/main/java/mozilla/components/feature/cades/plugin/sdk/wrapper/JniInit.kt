aadf76edd9f38bbd22270470aed7ed301684dfb4
Nedlog7 <nedlog7@rambler.ru>
2024-10-09 16:22:56 +0300

    Добавлены вызовы сишных методов установки лицензии, сертификатов и pfx.

--- mobile/android/android-components/components/feature/cades-plugin-sdk/src/main/java/mozilla/components/feature/cades/plugin/sdk/wrapper/JniInit.kt
+++ mobile/android/android-components/components/feature/cades-plugin-sdk/src/main/java/mozilla/components/feature/cades/plugin/sdk/wrapper/JniInit.kt
@@ -2,6 +2,7 @@ package mozilla.components.feature.cades.plugin.sdk.wrapper
 
 import android.content.Context
 import android.net.Uri
+import mozilla.components.feature.cades.plugin.sdk.R
 import mozilla.components.support.base.log.logger.Logger
 import org.apache.commons.codec.binary.Base32
 import org.apache.commons.codec.binary.Base64
@@ -12,7 +13,6 @@ import ru.cprocsp.URIManager.LicenseManager
 import ru.cprocsp.URIManager.PFXManager
 import ru.cprocsp.URIManager.RootCertificateManager
 import ru.cprocsp.URIManager.URIManagerFactory
-import ru.cprocsp.qrscanner.R
 
 class JniInit {
     companion object {
@@ -22,6 +22,10 @@ class JniInit {
 
         private const val CADES_OCSP_LICENSE = "0A20B-83010-00KAN-9Q3BW-8EQDV"
         private const val CADES_TSP_LICENSE = "TA20D-H3010-00KAN-GF6KF-MVN4R"
+
+        private const val RESULT_INSTALL_OK = -1
+        private const val RESULT_INSTALL_ERROR = -2
+        private const val RESULT_ERROR_INVALID_PASSWORD: Int = -3
 
         @JvmStatic
         fun initNativeCSP(context: Context) {
@@ -57,27 +61,52 @@ class JniInit {
             try {
                 val uriManager = URIManagerFactory.getInstance(uri, context)
                 val listener: URIManagerFactory.ContentListener = URIManagerFactory.ContentListener { content, _ ->
-                    content?.let {
-                        //val base32 = Base32()
-                        //val base64 = Base64()
-                        //val base64Data = base64.encode(base32.decode(it))
+                    content?.let { it ->
+                        var messageId: Int = -1
+                        var isError = false
+                        val base32 = Base32()
+                        val base64 = Base64()
+                        val base64Data = base64.encodeAsString(base32.decode(it))
                         when(uriManager) {
                             is RootCertificateManager -> {
+                                val resultCode = JniWrapper.installRootCert(base64Data)
+                                if (resultCode == RESULT_INSTALL_OK) {
+                                    messageId = R.string.cert_installation_success
+                                } else {
+                                    messageId = R.string.cert_installation_failed
+                                    isError = true
+                                }
                             }
                             is CRLManager -> {
-
                             }
                             is IntermediateCertificateManager -> {
-
                             }
                             is PFXManager -> {
-
+                                val resultCode = JniWrapper.installPfx(base64Data, "")
+                                when(resultCode) {
+                                    RESULT_INSTALL_OK -> messageId = R.string.pfx_installation_success
+                                    RESULT_ERROR_INVALID_PASSWORD -> {}
+                                    else -> {
+                                        messageId = R.string.pfx_installation_failed
+                                        isError = true
+                                    }
+                                }
                             }
                             is LicenseManager -> {
-
+                                val resultCode = JniWrapper.licenseCsp(base64Data, "", "")
+                                if (resultCode == RESULT_INSTALL_OK) {
+                                    messageId = R.string.license_installation_success
+                                } else {
+                                    messageId = R.string.license_installation_failed
+                                    isError = true
+                                }
                             }
-                            else -> onShowSnackbar(context.getString(R.string.InvalidURIFormat), true)
+                            else -> {
+                                messageId = R.string.InvalidURIFormat
+                                isError = true
+                            }
                         }
+                        if (messageId != -1) onShowSnackbar(context.getString(messageId), isError)
                     }
                 }
                 uriManager.setListener(listener)
